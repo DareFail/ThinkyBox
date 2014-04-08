bc15846a9fb98624027d634ffbbd3d9daa018dcf
jamespsteinberg <jamespsteinberg@gmail.com>
2014-04-08 19:43:28 -0400

    Initial Commit

---
+++ ThinkBox/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ThinkBox
+//
+//  Created by James Steinberg on 4/8/14.
+//  Copyright (c) 2014 ___JamesSteinberg___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
