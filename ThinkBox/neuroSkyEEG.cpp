ad96f41f5eb5fe9f430f937184fcb8e759363c2a
jamespsteinberg <jamespsteinberg@gmail.com>
2014-04-11 10:33:47 -0400

    created modular eeg sensor class

---
+++ ThinkBox/neuroSkyEEG.cpp
@@ -0,0 +1,128 @@
+//
+//  neuroSkyEEG.cpp
+//  ThinkBox
+//
+//  Created by James Steinberg on 4/11/14.
+//  Copyright (c) 2014 ___JamesSteinberg___. All rights reserved.
+//
+
+#include "neuroSkyEEG.h"
+
+int connectionID = -1;        // ThinkGear connection handle
+CFURLRef bundleURL;           // path reference to bundle
+CFBundleRef thinkGearBundle;  // bundle reference
+
+int (*TG_Disconnect)(int) = NULL;
+void (*TG_FreeConnection)(int) = NULL;
+
+
+/**
+ * This function handles signal interrupts.
+ *
+ * Basically perform cleanup on the objects and then exit the program.
+ */
+void siginthandler(int sig){
+    fprintf(stderr, "\nDisconnecting...\n");
+    
+    // close the connection
+    if(connectionID != -1){
+        TG_Disconnect(connectionID);
+        TG_FreeConnection(connectionID);
+    }
+    
+    // release the bundle references
+    if(bundleURL)
+        CFRelease(bundleURL);
+    
+    if(thinkGearBundle)
+        CFRelease(thinkGearBundle);
+    
+    exit(1);
+}
+
+bool NeuroSkyEEG::initialize(void)
+{
+    retVal = -1;                       // return values from TG functions
+    
+    numPackets = 0;                    // number of packets returned from ReadPackets
+    signalQuality = 0.0;             // poor signal status 1-255
+    attention = 0.0;                 // eSense attention 1-100
+    meditation = 0.0;                // eSense meditation 1-100
+    didBlink = 0.0;             // eSense blinking  0 or 1
+    blinkStrength = 0.0;          // strength of the blink 1-255
+    
+    // register the signal interrupt handler
+    signal(SIGINT, siginthandler);
+    
+    srand(time(NULL));
+    
+    // create the path reference to the bundle
+    bundleURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault,
+                                              CFSTR("/Users/jamespsteinberg/Documents/ThinkBox/ThinkBox/ThinkGear.bundle"),
+                                              kCFURLPOSIXPathStyle,
+                                              true);
+    
+    // create the bundle reference
+    thinkGearBundle = CFBundleCreate(kCFAllocatorDefault, bundleURL);
+    
+    // make sure the bundle actually exists
+    if(!thinkGearBundle){
+        fprintf(stderr, "Error: Could not find ThinkGear.bundle. Does it exist in the current directory?\n");
+        exit(1);
+    }
+    
+    // now start setting the function pointers
+    TG_GetDriverVersion =   (int (*)())CFBundleGetFunctionPointerForName(thinkGearBundle, CFSTR("TG_GetDriverVersion"));
+    TG_GetNewConnectionId = (int (*)())CFBundleGetFunctionPointerForName(thinkGearBundle, CFSTR("TG_GetNewConnectionId"));
+    TG_Connect =            (int (*)(int, const char *, int, int))CFBundleGetFunctionPointerForName(thinkGearBundle, CFSTR("TG_Connect"));
+    TG_ReadPackets =        (int (*)(int, int))CFBundleGetFunctionPointerForName(thinkGearBundle, CFSTR("TG_ReadPackets"));
+    TG_GetValue =           (float (*)(int, int))CFBundleGetFunctionPointerForName(thinkGearBundle, CFSTR("TG_GetValue"));
+    TG_Disconnect =         (int (*)(int))CFBundleGetFunctionPointerForName(thinkGearBundle, CFSTR("TG_Disconnect"));
+    TG_FreeConnection =     (void (*)(int))CFBundleGetFunctionPointerForName(thinkGearBundle, CFSTR("TG_FreeConnection"));
+    TG_EnableBlinkDetection =     (int (*)(int, int))CFBundleGetFunctionPointerForName(thinkGearBundle, CFSTR("TG_EnableBlinkDetection"));
+    TG_GetValueStatus =          (int (*)(int, int))CFBundleGetFunctionPointerForName(thinkGearBundle, CFSTR("TG_GetValueStatus"));
+    
+    // check for any invalid function pointers
+    if(!TG_GetDriverVersion || !TG_GetNewConnectionId || !TG_Connect || !TG_ReadPackets ||
+       !TG_GetValue || !TG_Disconnect || !TG_FreeConnection || !TG_EnableBlinkDetection || !TG_GetValueStatus){
+        fprintf(stderr, "Error: Expected functions in ThinkGear.bundle were not found. Are you using the right version?\n");
+        exit(1);
+    }
+    
+    // get the connection ID
+    connectionID = TG_GetNewConnectionId();
+    
+    fprintf(stderr, "Connecting to %s ... ", portname);
+    
+    // attempt to connect
+    retVal = TG_Connect(connectionID, portname, TG_BAUD_9600, TG_STREAM_PACKETS);
+
+    return retVal;
+}
+
+
+void NeuroSkyEEG::setEEGData()
+{
+    // read the packets from the output stream
+    numPackets = TG_ReadPackets(connectionID, -1);
+    
+    // check whether we've received any new packets
+    if(numPackets > 0){
+        // if so, parse them
+        signalQuality = TG_GetValue(connectionID, TG_DATA_POOR_SIGNAL);
+        attention = TG_GetValue(connectionID, TG_DATA_ATTENTION);
+        meditation = TG_GetValue(connectionID, TG_DATA_MEDITATION);
+        didBlink = TG_GetValueStatus(connectionID, TG_DATA_BLINK_STRENGTH);
+        blinkStrength = TG_GetValue(connectionID, TG_DATA_BLINK_STRENGTH);
+        
+        // then output everything
+        fprintf(stdout, "\rPoorSig: %3.0f, Att: %3.0f, Med: %3.0f, Blk: %3.0f, blkstrgth: %3.0f", signalQuality, attention, meditation, didBlink, blinkStrength);
+        fflush(stdout);
+    }
+}
+
+void NeuroSkyEEG::enableBlink()
+{
+    // enable blink tracking
+    TG_EnableBlinkDetection(connectionID, 1 );
+}
---
+++ ThinkBox/neuroSkyEEG.h
@@ -0,0 +1,94 @@
+//
+//  neuroSkyEEG.h
+//  ThinkBox
+//
+//  Created by James Steinberg on 4/11/14.
+//  Copyright (c) 2014 ___JamesSteinberg___. All rights reserved.
+//
+
+#ifndef __ThinkBox__neuroSkyEEG__
+#define __ThinkBox__neuroSkyEEG__
+
+#include <iostream>
+#include <CoreFoundation/CoreFoundation.h>
+
+class NeuroSkyEEG
+{
+private:
+    /**
+     * Baud rate for use with TG_Connect() and TG_SetBaudrate().
+     */
+#define TG_BAUD_1200         1200
+#define TG_BAUD_2400         2400
+#define TG_BAUD_4800         4800
+#define TG_BAUD_9600         9600
+#define TG_BAUD_57600       57600
+#define TG_BAUD_115200     115200
+    
+    /**
+     * Data format for use with TG_Connect() and TG_SetDataFormat().
+     */
+#define TG_STREAM_PACKETS      0
+#define TG_STREAM_5VRAW        1
+#define TG_STREAM_FILE_PACKETS 2
+    
+    /**
+     * Data type that can be requested from TG_GetValue().
+     */
+#define TG_DATA_BATTERY      0
+#define TG_DATA_POOR_SIGNAL  1
+#define TG_DATA_ATTENTION    2
+#define TG_DATA_MEDITATION   3
+#define TG_DATA_RAW          4
+#define TG_DATA_DELTA        5
+#define TG_DATA_THETA        6
+#define TG_DATA_ALPHA1       7
+#define TG_DATA_ALPHA2       8
+#define TG_DATA_BETA1        9
+#define TG_DATA_BETA2       10
+#define TG_DATA_GAMMA1      11
+#define TG_DATA_GAMMA2      12
+#define TG_DATA_BLINK_STRENGTH 37
+    
+    
+    /*
+     * ThinkGear function pointers
+     */
+    
+    int (*TG_GetDriverVersion)() = NULL;
+    int (*TG_GetNewConnectionId)() = NULL;
+    int (*TG_Connect)(int, const char *, int, int) = NULL;
+    int (*TG_ReadPackets)(int, int) = NULL;
+    float (*TG_GetValue)(int, int) = NULL;
+    int (*TG_EnableBlinkDetection)(int, int) = NULL;
+    int (*TG_GetValueStatus)(int, int) = NULL;
+    
+    const char * portname = "/dev/tty.MindWaveMobile-DevA";       // port name used to be argv[1]
+    int retVal;                       // return values from TG functions
+    
+    int numPackets;                    // number of packets returned from ReadPackets
+    float signalQuality;             // poor signal status 1-255
+    float attention;                 // eSense attention 1-100
+    float meditation;                // eSense meditation 1-100
+    float didBlink;             // eSense blinking  0 or 1
+    float blinkStrength;          // strength of the blink 1-255
+
+public:
+    
+    bool initialize(void);
+    void setEEGData(void);
+    void enableBlink(void);
+    
+    // getters
+    float getSignalQuality(void) { return signalQuality; };             // poor signal status 1-255
+    float getAttention(void) { return attention; };                 // eSense attention 1-100
+    float getMeditation(void) { return meditation; };                // eSense meditation 1-100
+    float getDidBlink(void) { return didBlink; };             // eSense blinking  0 or 1
+    float getBlinkStrength(void) { return blinkStrength; };          // strength of the blink 1-255
+};
+
+void siginthandler(int sig);
+
+#endif /* defined(__ThinkBox__neuroSkyEEG__) */
+
+
